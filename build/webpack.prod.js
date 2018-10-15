794249c93a385d77703e5939a20b992b4efe4b39
liangjunsong <liangjun_song@yeah.net>
2018-10-15 11:30:42 +0800

    add mini-css-extract-plugin

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -1,6 +1,7 @@
 // prod环境专用配置
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = merge(common, {
   mode: 'production',
@@ -11,7 +12,7 @@ module.exports = merge(common, {
         exclude: [/node_modules/],
         use: [
           { 
-            loader: "style-loader",
+            loader: MiniCssExtractPlugin.loader ,
           },
           {
             loader: "css-loader",
@@ -25,4 +26,10 @@ module.exports = merge(common, {
       }
     ]
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].[hash].css',
+      chunkFilename: '[id].[hash].css',
+    })
+  ],
 });
